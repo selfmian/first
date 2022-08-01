edc5778ae032c5b2a8550717f98987f86eb3995f
薛皓腾 <11252787+xuehaoteng@user.noreply.gitee.com>
2022-08-01 09:02:17 +0800

    first conmit

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login-container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Login",
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.login-container{
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    background-color: #235bae;
+    background-image: url(../assets/login_bg.a62d8a.jpg);
+    background-size: cover;
+}
+</style>
